0cbcf3ca2ad7f9ceafb2f0e270fd46f33be01cb9
MUDUSU NEELANJANA <37610561+neelanjanamudusu@users.noreply.github.com>
2020-05-04 14:34:08 +0530

    commit

--- UpdateAddress/src/test/java/com/capgemini/UpdateAddress/UpdateAddressApplicationTests.java
+++ UpdateAddress/src/test/java/com/capgemini/UpdateAddress/UpdateAddressApplicationTests.java
@@ -1,13 +1,12 @@
-package com.capgemini.UpdateAddress;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class UpdateAddressApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+/*
+ * package com.capgemini.UpdateAddress;
+ * 
+ * import org.junit.jupiter.api.Test; import
+ * org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * @SpringBootTest class UpdateAddressApplicationTests {
+ * 
+ * @Test void contextLoads() { }
+ * 
+ * }
+ */
