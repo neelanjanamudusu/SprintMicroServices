42098845aab23e4a966ed57bcee5af2d6d6cdb3b
MUDUSU NEELANJANA <37610561+neelanjanamudusu@users.noreply.github.com>
2020-05-04 15:35:30 +0530

    commit

--- SpringCloudConfigServer/src/test/java/com/capgemini/SpringCloudConfigServerApplicationTests.java
+++ SpringCloudConfigServer/src/test/java/com/capgemini/SpringCloudConfigServerApplicationTests.java
@@ -1,13 +1,12 @@
-package com.capgemini;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SpringCloudConfigServerApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+/*
+ * package com.capgemini;
+ * 
+ * import org.junit.jupiter.api.Test; import
+ * org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * @SpringBootTest class SpringCloudConfigServerApplicationTests {
+ * 
+ * @Test void contextLoads() { }
+ * 
+ * }
+ */
