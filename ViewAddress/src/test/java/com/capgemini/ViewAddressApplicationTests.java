98fb36951e6a2998fce835c9f2fdcb799c475a45
MUDUSU NEELANJANA <37610561+neelanjanamudusu@users.noreply.github.com>
2020-05-04 14:35:56 +0530

    commit

--- ViewAddress/src/test/java/com/capgemini/ViewAddressApplicationTests.java
+++ ViewAddress/src/test/java/com/capgemini/ViewAddressApplicationTests.java
@@ -1,13 +1,12 @@
-package com.capgemini;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ViewAddressApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+/*
+ * package com.capgemini;
+ * 
+ * import org.junit.jupiter.api.Test; import
+ * org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * @SpringBootTest class ViewAddressApplicationTests {
+ * 
+ * @Test void contextLoads() { }
+ * 
+ * }
+ */
