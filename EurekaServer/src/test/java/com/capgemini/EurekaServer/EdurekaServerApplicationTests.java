d4cc2bca45da96edb52f9b965be87a0f6db7abe0
MUDUSU NEELANJANA <37610561+neelanjanamudusu@users.noreply.github.com>
2020-05-04 15:32:42 +0530

    commit

--- EurekaServer/src/test/java/com/capgemini/EurekaServer/EdurekaServerApplicationTests.java
+++ EurekaServer/src/test/java/com/capgemini/EurekaServer/EdurekaServerApplicationTests.java
@@ -1,13 +1,12 @@
-package com.capgemini.EurekaServer;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class EurekaServerApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+/*
+ * package com.capgemini.EurekaServer;
+ * 
+ * import org.junit.jupiter.api.Test; import
+ * org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * @SpringBootTest class EurekaServerApplicationTests {
+ * 
+ * @Test void contextLoads() { }
+ * 
+ * }
+ */
